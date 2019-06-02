2fb20c507328410f2718b158de254884f2faaef7
Alejandro Alcalde <algui91@gmail.com>
2019-06-02 22:06:42 +0200

    Added missing LOCAL_SDK_VERSION
    Change-Id: I94afa7607476c6cec37e8c09ee0e6aba5e71c2db

--- SonyOtgSwitch/Android.mk
+++ SonyOtgSwitch/Android.mk
@@ -3,17 +3,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    $(call all-java-files-under, src)
+	$(call all-java-files-under, src)
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res
+	$(LOCAL_PATH)/res
 
 LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay
+	--auto-add-overlay
 
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := SonyOtgSwitch
+LOCAL_SDK_VERSION := system_current
 
 include $(BUILD_PACKAGE)
